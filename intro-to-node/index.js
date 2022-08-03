fe1bcbfcdb60500a1d6cd344c255ebbbbb0bf8cb
John Sanjaya <89222969+JSanjaya@users.noreply.github.com>
2022-08-03 14:58:46 -0700

    Add files via upload
    Intro to Node.JS

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+console.log(supervillains.random());
